cfe22417502609c2da737d4258cc9ff3ebdabfd5
Braden Filler <braden.filler@siriuscom.com>
2017-07-20 15:18:50 -0500

    i did something

---
+++ src/app/forum-message/forum-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forum-message',
+  templateUrl: './forum-message.component.html',
+  styleUrls: ['./forum-message.component.scss']
+})
+export class ForumMessageComponent {
+  title = 'app';
+}
