cea5dcabf770f91f6b81b85d7671fd0402c3d0c9
Braden Filler <braden.filler@siriuscom.com>
2017-07-20 16:57:14 -0500

    fixed conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { ForumMessageComponent } from './forum-message/forum-message.component';
 import { ForumMessageContainerComponent } from './forum/forum-message-container.component';
 import { ForumModalComponent } from './forum-modal/forum-modal.component';
+import { ProfileListComponent } from './profile-list/profile-list.component';
+import { HomeComponent } from './home/home.component';
+import { FullProfileComponent } from './full-profile/full-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ForumMessageComponent,
     ForumMessageContainerComponent,
-    ForumModalComponent
+    ForumModalComponent,
+    ProfileListComponent,
+    HomeComponent,
+    FullProfileComponent
   ],
   imports: [
     BrowserModule,
