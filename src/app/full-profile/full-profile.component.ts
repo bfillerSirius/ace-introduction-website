fdc429e4869e6f3c7150b53b95925681f591197f
Christopher Harlow <Christopher.Harlow@siriuscom.com>
2017-07-20 15:20:21 -0500

    file Structure

---
+++ src/app/full-profile/full-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-profile',
+  templateUrl: './full-profile.component.html',
+  styleUrls: ['./full-profile.component.scss']
+})
+export class FullProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
