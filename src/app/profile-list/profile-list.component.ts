918280e609f4d79e9b494b1f5e1809c54959fa84
Christopher Harlow <Christopher.Harlow@siriuscom.com>
2017-07-21 16:00:33 -0500

    implemented profile-list

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, } from '@angular/core';
+import {EMPLOYEES, DIRECTORS} from '../../assets/people'
+// import {NgFor} from '@angular/'
 @Component({
   selector: 'app-profile-list',
   templateUrl: './profile-list.component.html',
   styleUrls: ['./profile-list.component.scss']
 })
 export class ProfileListComponent implements OnInit {
-
+  myEmployees = EMPLOYEES;
+  myDirectors =DIRECTORS;
   constructor() { }
 
   ngOnInit() {
