7d35b2ee15019cbc4b0978e232ea63b607b1301c
Jenifer Sebastian <jen.sebastian@siriuscom.com>
2017-07-22 16:38:36 -0500

    fixed styling home profile list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
 
   openNav(){
   document.getElementById('sideNavBar').style.width ="180px"
-  document.getElementById('sideNavBar').style.border ="solid 2px #ffa000"
+  document.getElementById('sideNavBar').style.border ="solid 2px lightgray"
 }
 closeNav(){
   document.getElementById('sideNavBar').style.width ="0"
